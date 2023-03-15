56751e0841a35e098c99369e41061b8955027eea
Marcus Groß <marcus.gross@inwt-statistics.de>
2023-03-15 18:11:59 +0100

    merge fix import

--- inst/app/server.R
+++ inst/app/server.R
@@ -30,6 +30,21 @@ shinyServer(function(input, output, session) {
       as.matrix()
   }) %>%
     bindEvent(importedData())
+
+  dat$dataFileSD <- eventReactive(input$fileDataSD, ignoreNULL = TRUE, {
+    inFile <- input$fileDataSD
+    if (is.null(inFile)) return(NULL)
+    file <- inFile$datapath
+    if(grepl(".csv$", file)){
+      name <- read.csv(file, sep = input$colseparatorData, dec = input$decseparatorData)
+    } else if(grepl(".xlsx$", file)){
+      name <- read.xlsx(file, sheetIndex = 1)
+    }
+    
+    if(any(sapply(as.matrix(name), is.character))) { shinyjs::alert("Please provide a dataset with all numeric variables.") }
+    name <- name[, !(apply(name, 2, function(x) all(is.na(x))))]
+    as.matrix(name)
+  })
   
   importedIso <- DataTools::importDataServer(
     "fileIso",
@@ -61,17 +76,20 @@ shinyServer(function(input, output, session) {
     updateMatrixNamesInput(session, "dataMatrixSD", value = input$dataMatrix, value2 = input$dataMatrixSD)
   })
   
+
   observeEvent(dat$dataFile, {
     updateMatrixInput(session, "dataMatrix", value = dat$dataFile)
-    browser()
-    #updateMatrixInput(session, "dataMatrixSD", value = dat$dataFile(inputId = input$fileDataSD) )
-
-  # observeEvent(input$fileData, {
-  #   updateMatrixInput(session, "dataMatrix", value = dat$dataFile() )
-  #   updateMatrixInput(session, "dataMatrixSD", value = dat$dataFile(inputId = input$fileDataSD) )
     
     # reset values storing data from uploaded models
     uploadedDataMatrix(NULL)
+    uploadedIsotope(NULL)
+    uploadedModelSpecInputs(NULL)
+  })
+  
+  observeEvent(input$fileDataSD, {
+    updateMatrixInput(session, "dataMatrixSD", value = dat$dataFileSD())
+    
+    # reset values storing data from uploaded models
     uploadedDataMatrixSD(NULL)
     uploadedIsotope(NULL)
     uploadedModelSpecInputs(NULL)
@@ -322,6 +340,7 @@ shinyServer(function(input, output, session) {
     newModel <- setNames(list(
       list(modelSpecifications = reactiveValuesToList(modelSpecInputs()),
            inputDataMatrix = input$dataMatrix,
+           inputDataMatrixSD = input$inputDataMatrixSD,
            inputIsotope = input$isotope,
            fit = fitToSave)
     ), input$modelName)
